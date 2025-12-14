42fce587c819172c92e3f1fdda3164658a5897a8
Yusuf Emir Tolan <yusufemirtolan@outlook.com>
2025-12-14 10:35:40 +0300

    Add files via upload

---
+++ numpy_estate_project.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+m2 = np.random.randint(70,201,1000)
+nmbr_room = np.random.randint(2,7,1000)
+district_code = np.random.randint(0,3,1000)
+prices = (m2 * 5000) + (nmbr_room* 15000) + (district_code * 30000)
+print (f"\nAverage prices of houses:{np.mean(prices)}")
+print (f"\nThe median of prices of houses:{np.median(prices)}")
+print (f"\nRisk of prices of houses:{np.std(prices)}")
+
+cheap_filter = (district_code == 0) & (prices < np.mean(prices))
+
+how_many = np.sum(cheap_filter)
+
+print(f"\nOpportunity Houses Count: {how_many}")
